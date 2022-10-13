cd576dc30cf78d045e79e6c5858e4e1a173ce9b4
achyut kadam <achyut.k@acldigital.com>
2022-10-13 17:08:10 +0530

    added urls of login page.

--- crudexample/urls.py
+++ crudexample/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('employee.urls'))
+    path('login/', include('employee.urls')),
+    # path('login/',include('home.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 ]
